601b4847ac1243b4fc6a2f42ec8e1760dfa65805
TeamCoda-sys <80285777+TeamCoda-sys@users.noreply.github.com>
2021-04-26 13:36:33 +0200

    Add files via upload

---
+++ MyArchiveFile.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'aW1wb3J0IG9zDQppbXBvcnQgc3lzDQpmcm9tIHppcGZpbGUgaW1wb3J0IFppcEZpbGUNCnByaW50KCJNeUFyY2hpdmVGaWxlIDEuMCIpDQpwcmludCgiXG5DcmVhdGUgeW91ciBvd24gYXJjaGl2'
+love = 'MFOznJkyVUEiMTS5VPSpovVcQDcuVQ0tnJ5jqKDbVx5uoJHto2LtrJ91pvOwqKA0o20tLKWwnTy2MFOznJkyVQbtVvxAPzVtCFOcoaO1qPtvEKu0MJ5mnJ9hVT9zVUyiqKVtL3ImqT9gVTSlL2uc'
+god = 'dmUgZmlsZSA6ICIpDQppZiBiID09ICdqcGcnOg0KICAgIHByaW50KCJOTyAhIikNCiAgICBleGl0KCkNCmlmIGIgPT0gJ2pwZWcnOg0KICAgIHByaW50KCJOTyAhIikNCiAgICBleGl0KCkNCmlm'
+destiny = 'VTVtCG0tW3OhMlp6QDbtVPNtpUWcoaDbVx5CVPRvXD0XVPNtVTI4nKDbXD0XMJkmMGbAPvNtVPO6nKOCLzbtCFOnnKOTnJkyXTRtXlNaYvptXlOvYPNaqlpcQDbtVPNtrzyjG2WdYzAfo3AyXPx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
